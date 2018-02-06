d4b2683a14492ce0f2539acb2ab1aa9b964799d0
Alexandre Ouellet <AOuellet@square-enix-montreal.com>
2018-02-06 12:49:18 -0500

    Initial packages for a Snake environment.  Currently is a copy/paste of the cartpole environment

--- gym_snake/__init__.py
+++ gym_snake/__init__.py
@@ -1,9 +1,7 @@
-import logging
 from gym.envs.registration import register
 
-logger = logging.getLogger(__name__)
 
 register(
-    id='Snake-v0',
-    entry_point='gym_snake.envs:SnakeEnv',
+    id='snake-v0',
+    entry_point='gym_snake.envs:SnakeEnv'
 )
--- gym_snake/envs/__init__.py
+++ gym_snake/envs/__init__.py
@@ -1,1 +1,1 @@
-from gym_snake.envs.snake_env import SnakeEnv
+from gym_snake.envs.snake_env import *
